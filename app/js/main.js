6c68f37d2b8b5243b8898850a811f5caa080ec64
vladkudinov89 <vlad.kudinov.89@gmail.com>
2017-08-30 22:33:40 +0300

    main work with style and portfolio-list

--- app/js/main.js
+++ app/js/main.js
@@ -77,14 +77,14 @@
          /* ---------------------------------------------- */
 
         /*Working without this*/
-        $('a[href="*"]').bind("click", function(e){
+        /*$('a[href*="#"]').bind("click", function(e){
 
             var anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top
             }, 1000);
             e.preventDefault();
-        });
+        });*/
 
         $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
