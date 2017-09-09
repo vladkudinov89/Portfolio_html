bf664df3c1a697d1a5f9359a491229f505014515
vladkudinov89 <vlad.kudinov.89@gmail.com>
2017-09-09 17:41:31 +0300

    fix problem with typed.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 (function ($) {
 
+
     /* ---------------------------------------------- /*
      * Preloader
      /* ---------------------------------------------- */
@@ -38,6 +39,33 @@
     });
 
     $(document).ready(function () {
+
+        if( window.location.pathname == '/' ){
+            var typed = new Typed(".cur-point", {
+
+                strings: ['FRONT-END ^500DEVELOPER '],
+                stringsElement: null,
+                typeSpeed: 150,
+                startDelay: 2000,
+                fadeOut: true,
+                fadeOutClass: 'typed-fade-out',
+                fadeOutDelay: 500,
+                loop: false,
+                loopCount: Infinity,
+                showCursor: true,
+                cursorChar: '|',
+                autoInsertCss: true,
+                contentType: 'html',
+                onComplete: function(self){
+                    "use strict";
+                    $('.typed-cursor').css({
+                        "transition":"opacity 1s linear",
+                        "opacity" : "0"
+                    });
+                }
+
+            });
+        }//if
 
         $(".nano").nanoScroller();
 
@@ -181,3 +209,4 @@
     });
 
 })(jQuery);
+
